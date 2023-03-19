816b703e5ca4e56315711ae8a14e571496a4034c
lmw_Desktop <lmw_Desktop@DESKTOP-QF8AMST>
2023-03-19 12:29:06 +0900

    게시판 추가

---
+++ AspnetNote/Controllers/NoteController.cs
@@ -0,0 +1,91 @@
+﻿using AspnetNote.DataContext;
+using AspnetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNote.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시판 리스트 
+        /// </summary>
+
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            //var list = new List<Note>();
+            using (var db = new AspnetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }            
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Notes.Add(model);
+                    if(db.SaveChanges() > 0) //몇개가 성공했는지 개수가 반환값으로 넘어옴
+                    {
+                        return Redirect("Index");   //RedirectToAction("Index","Note"); //다른 컨트롤러 view로 갈때 사용
+                    }                       
+                }
+                ModelState.AddModelError(string.Empty, "게시물을 저장할 수 없습니다.");
+            }
+            return View(model);
+        }
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
